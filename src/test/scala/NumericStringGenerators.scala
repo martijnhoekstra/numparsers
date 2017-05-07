9425a4fa3a766cfbb96250dd46a371ddbcc338ee
Martijn Hoekstra <MartijnHoekstra@gmail.com>
2017-05-07 21:51:20 +0200

    fix generator signum

--- src/test/scala/NumericStringGenerators.scala
+++ src/test/scala/NumericStringGenerators.scala
@@ -6,52 +6,52 @@ object NumericStringGenerators {
 
   val nearOverflowByteUpper = {
     val lower: Int = Byte.MaxValue.toInt / 10
-    val upper: Int = Byte.MaxValue.toInt * 10
+    val upper: Int = Byte.MaxValue.toInt * 20
     Gen.choose(lower, upper).map(_.toString)
   }
 
   val nearOverflowByteLower = {
     val upper: Int = Byte.MinValue.toInt / 10
-    val lower: Int = Byte.MinValue.toInt * 10
+    val lower: Int = Byte.MinValue.toInt * 20
     Gen.choose(lower, upper).map(_.toString)
   }
 
   val nearOverflowShortUpper = {
     val lower: Int = Short.MaxValue.toInt / 10
-    val upper: Int = Short.MaxValue.toInt * 10
+    val upper: Int = Short.MaxValue.toInt * 20
     Gen.choose(lower, upper).map(_.toString)
   }
 
 
   val nearOverflowShortLower = {
     val upper: Int = Short.MinValue.toInt / 10
-    val lower: Int = Short.MinValue.toInt * 10
+    val lower: Int = Short.MinValue.toInt * 20
     Gen.choose(lower, upper).map(_.toString)
   }
 
   val nearOverflowIntUpper = {
     val lower: Long = Int.MaxValue.toLong / 10
-    val upper: Long = Int.MaxValue.toLong * 10
+    val upper: Long = Int.MaxValue.toLong * 20
     Gen.choose(lower, upper).map(_.toString)
   }
 
   val nearOverflowIntLower = {
     val upper: Long = Int.MinValue.toLong / 10
-    val lower: Long = Int.MinValue.toLong * 10
+    val lower: Long = Int.MinValue.toLong * 20
     Gen.choose(lower, upper).map(_.toString)
   }
 
   val nearOverflowLongUpper = {
     val base = (Long.MaxValue / 10).toString
     for {
-      end <- Gen.choose(0, 100)
+      end <- Gen.choose(0, 200)
     } yield base + end.toString
   }
 
   val nearOverflowLongLower = {
     val base = (Long.MinValue / 10).toString
     for {
-      end <- Gen.choose(0, 100)
+      end <- Gen.choose(0, 200)
     } yield base + end.toString
   }
 
@@ -61,12 +61,12 @@ object NumericStringGenerators {
   val overflowShortLower = Gen.choose(Int.MinValue, Short.MinValue.toInt - 1).map(_.toString)
   val overflowIntUpper = Gen.choose(Int.MaxValue.toLong + 1, Long.MaxValue).map(_.toString)
   val overflowIntLower = Gen.choose(Long.MinValue, Int.MinValue.toLong - 1).map(_.toString)
-  
+
   val validBytes = 
     for {
       byte <- Gen.choose(Byte.MinValue, Byte.MaxValue)
       zeroes <- Gen.listOf(Gen.const('0')).map(_.mkString)
-      signum <- Gen.oneOf(List("+", "-", "").filterNot(s => (s == "+" && byte < 0) || (s == "-" && byte > 0)))
+      signum <- if (byte > 0) Gen.oneOf(List("+", "")) else if (byte == 0) Gen.oneOf(List("+", "", "-")) else Gen.const("-")
     } yield {
       val numbers = if (byte >= 0) byte.toString else byte.toString.substring(1)
       signum + zeroes + numbers
@@ -76,7 +76,7 @@ object NumericStringGenerators {
     for {
       short <- Gen.choose(Short.MinValue, Short.MaxValue)
       zeroes <- Gen.listOf(Gen.const('0')).map(_.mkString)
-      signum <- Gen.oneOf(List("+", "-", "").filterNot(s => (s == "+" && short < 0) || (s == "-" && short > 0)))
+      signum <- if (short > 0) Gen.oneOf(List("+", "")) else if (short == 0) Gen.oneOf(List("+", "", "-")) else Gen.const("-")
     } yield {
       val numbers = if (short >= 0) short.toString else short.toString.substring(1)
       signum + zeroes + numbers
@@ -86,7 +86,7 @@ object NumericStringGenerators {
     for {
       int <- Gen.choose(Int.MinValue, Int.MaxValue)
       zeroes <- Gen.listOf(Gen.const('0')).map(_.mkString)
-      signum <- Gen.oneOf(List("+", "-", "").filterNot(s => (s == "+" && int < 0) || (s == "-" && int > 0)))
+      signum <-if (int > 0) Gen.oneOf(List("+", "")) else if (int == 0) Gen.oneOf(List("+", "", "-")) else Gen.const("-")
     } yield {
       val numbers = if (int >= 0) int.toString else int.toString.substring(1)
       signum + zeroes + numbers
@@ -96,7 +96,7 @@ object NumericStringGenerators {
     for {
       long <- Gen.choose(Long.MinValue, Long.MaxValue)
       zeroes <- Gen.listOf(Gen.const('0')).map(_.mkString)
-      signum <- Gen.oneOf(List("+", "-", "").filterNot(s => (s == "+" && long < 0) || (s == "-" && long > 0)))
+      signum <- if (long > 0) Gen.oneOf(List("+", "")) else if (long == 0) Gen.oneOf(List("+", "", "-")) else Gen.const("-")
     } yield {
       val numbers = if (long >= 0) long.toString else long.toString.substring(1)
       signum + zeroes + numbers
